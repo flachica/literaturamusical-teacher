545b82e21bdaf61658e9d228ff2c1346cd562639
Fernando La Chica <flachica@sdi.es>
2026-08-14 20:39:56 +0200

    feat: pasar prop audioStatus a SongManager en ModoAdmin.jsx

--- src/components/ModoAdmin.jsx
+++ src/components/ModoAdmin.jsx
@@ -133,6 +133,7 @@ export default function ModoAdmin({
       {pestanaActiva === 'canciones' && (
         <SongManager
           canciones={canciones}
+          audioStatus={audioStatus}
           onGuardarCanciones={onGuardarCanciones}
           onRestaurarDefault={onRestaurarCanciones}
         />
